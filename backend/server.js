d36ef3b01910ab2911e1b36ef6d3f06dda7eafd1
Sudarshan singh <sudarshans471@gmail.com>
2023-05-02 13:40:44 +1000

    added some more code in server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-const express = require ('express')
-const products =  require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
 
 const app = express()
 
@@ -16,4 +19,5 @@ app.get('/api/products/:id' , (req, res) => {
     res.json(product)
 })
 
-app.listen(3002, console.log('server running on port 3002'))
+const PORT = process.env.PORT || 3002
+app.listen(PORT, console.log(`server running IN ${process.env.NODE_ENV} MODE on port ${PORT}`))
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -9,12 +9,12 @@ const ProductScreen = ({ match }) => {
 
   useEffect(() =>{
     const fetchProduct = async () =>{
-      const { data } = await axios.get(`/api/products/${'match.params.id'}`)
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
 
       setProduct(data)
     }
     fetchProduct()
-  }, [])
+  }, [match])
 
   return (
     <>
